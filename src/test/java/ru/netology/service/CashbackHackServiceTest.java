bf5799a6cb7a0337c62873086df953e885d0343d
Laco test <lo25654@yandex.ru>
2023-07-28 19:35:41 +0300

    Commit 3

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,10 @@
 package ru.netology.service;
 
-import org.testng.annotations.Test;
 
-import static org.testng.Assert.assertEquals;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
